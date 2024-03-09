93e255dcb5d56f32be471b28233ca2c8b9027cf1
srinivasagatla@lyrostech.com <srinivasareddygatla09@gmail.com>
2024-03-09 13:09:00 +0530

    Modified code

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ export default function App() {
   // Array of items
   const items = [
     {
+      name: 'Black Coffee',
+      image: '/public/images/cofee.jpg',
+      price: 2.5
+    },
+    {
+      name: 'Cappuccino',
+      image: '/images/cofee.jpg',
+      price: 3
+    },
+    {
+      name: 'Hot choclate coffee',
+      image: '/ecommreact/images/cofee.jpg',
+      price: 2.6
+    },
+    {
+      name: 'Guava',
+      image: 'https://th.bing.com/th/id/OIP.gHHwNXzq_P6NpIo06jjaGAHaE8?w=278&h=186&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+      price: 3.5
+    },
+    {
       name: 'Mango',
-      image: 'https://th.bing.com/th/id/R.ab6c56605d3aa4bab914b20c75fdb213?rik=tDMv9WRYvt2OeA&riu=http%3a%2f%2fweknowyourdreams.com%2fimages%2fmango%2fmango-09.jpg&ehk=dv%2b5PHEII27LKRaPpFh5E2wEmhnx4NNH3UpGsS6wZ70%3d&risl=&pid=ImgRaw&r=0',
-      price: 300
+      image: 'https://th.bing.com/th/id/OIP.KNHJ3Zj5fMpWo1Hhs97uDwHaF7?rs=1&pid=ImgDetMain',
+      price: 2.6
     },
     {
-      name: 'Apple',
-      image: 'https://th.bing.com/th/id/OIP.-4RJk-TNzc0Ulbu6-Ogx_AHaFR?rs=1&pid=ImgDetMain',
-      price: 300
-    },
-    {
-      name: 'Orange',
-      image: 'https://th.bing.com/th/id/OIP.oVeiT4LzCXtk9JVBfN-gMQHaE7?rs=1&pid=ImgDetMain',
-      price: 300
-    },
-    // {
-    //   name: 'Guava',
-    //   image: 'https://th.bing.com/th/id/OIP.gHHwNXzq_P6NpIo06jjaGAHaE8?w=278&h=186&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-    //   price: 300
-    // },
-    // {
-    //   name: 'Mango',
-    //   image: 'https://th.bing.com/th/id/OIP.KNHJ3Zj5fMpWo1Hhs97uDwHaF7?rs=1&pid=ImgDetMain',
-    //   price: 300
-    // },
-    // {
-    //   name: 'Banana',
-    //   image: 'https://loe.org/content/2014-04-18/10-bananabunch.gif',
-    //   price: 300
-    // }
+      name: 'Banana',
+      image: 'https://loe.org/content/2014-04-18/10-bananabunch.gif',
+      price: 2.2
+    }
   ];
 
   return (
